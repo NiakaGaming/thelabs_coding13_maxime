6dbe61fd116c63af2d082b83b8108b6c8d4acf2c
NiakaGaming <niaka-ishigame@hotmail.com>
2021-01-05 10:50:49 +0100

    Home Page done

--- resources/views/pages/user/home/index.blade.php
+++ resources/views/pages/user/home/index.blade.php
@@ -3,4 +3,9 @@
 @section('content')
     @include('pages.user.home.banner.index')
     @include('pages.user.home.about.index')
+    @include('pages.user.home.testimonial.index')
+    @include('pages.user.home.service.index')
+    @include('pages.user.home.team.index')
+    @include('pages.user.home.promotion.index')
+    @include('partials.contact-form')
 @endsection
